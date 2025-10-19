43b8d48581772301926f45254177f01f31044a85
Nikolaos Paronis <nikolaosparonis@gmail.com>
2025-10-19 17:28:32 +0200

    added changes to cards, to add donwload functionality. Added resume to site

--- src/components/CardListSection/Card.tsx
+++ src/components/CardListSection/Card.tsx
@@ -5,26 +5,35 @@ interface Props {
     color: string;
     buttontext: string;
     buttonURL: string;
+    downloadFile: boolean;
+    downloadFileName: string;
 }
+
 
 const Card = ( props: Props ) => {
 
+    const downloadHref = props.downloadFile
+        ? `/src/data/${props.downloadFileName}`
+        : props.buttonURL;
 
-    return (
-        <div className={"card__bx card__bx__" + props.color}>
-            <div className="card__data">
-                <div className="card__icon">
-                    <i className={props.icon}></i>
-                </div>
-                <div className="card__content">
-                    <h3>{props.title}</h3>
-                    <p>{props.text}</p>
-                    <a href={props.buttonURL} target="_blank">
-                        {props.buttontext}
-                    </a>
+        return (
+            <div className={"card__bx card__bx__" + props.color}>
+                <div className="card__data">
+                    <div className="card__icon">
+                        <i className={props.icon}></i>
+                    </div>
+                    <div className="card__content">
+                        <h3>{props.title}</h3>
+                        <p>{props.text}</p>
+                        <a
+                            href={downloadHref}
+                            {...(props.downloadFile ? { download: props.downloadFileName } : { target: "_blank", rel: "noopener noreferrer" })}
+                        >
+                            {props.buttontext}
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
 }
 export default Card;
--- src/components/CardListSection/CardListSection.tsx
+++ src/components/CardListSection/CardListSection.tsx
@@ -5,10 +5,20 @@ import Cardsdata from '../../data/cards.json'
 import './Card.css'
 import './CardListSection.css'
 
+interface CardData {
+    color: string;
+    icon: string;
+    title: string;
+    text: string;
+    buttontext: string;
+    buttonURL: string;
+    downloadFile: boolean;
+    downloadFileName: string;
+}
+
 const CardListSection = () => {
-    const [cards, setCards] = useState([
-            {color: "", icon: "", title: "", text: "", buttontext: "", buttonURL: ""},
-    ]);
+    const [cards, setCards] = useState<CardData[]>([]);
+    
     useEffect( () => {
         setCards(Cardsdata.cardsdata)
     }, []);
@@ -16,14 +26,16 @@ const CardListSection = () => {
         <section id="cardlistSection" className='cardlist'>
             <div className='cardcontainer'>
                 {cards.map( (card) => 
-                    <Card 
-                        key={card.title} 
-                        title={card.title} 
-                        text={card.text} 
-                        icon={card.icon} 
-                        color={card.color} 
-                        buttontext={card.buttontext} 
+                    <Card
+                        key={card.title}
+                        title={card.title}
+                        text={card.text}
+                        icon={card.icon}
+                        color={card.color}
+                        buttontext={card.buttontext}
                         buttonURL={card.buttonURL}
+                        downloadFile={card.downloadFile}
+                        downloadFileName={card.downloadFileName}
                     />
                 )}
             </div>
